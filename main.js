34bba6045f3e24ee1beb630c57b420cf89b5d405
MaheshRath08 <maheshrath.2002@gmail.com>
2022-08-09 19:43:58 +0530

    2nd commit

--- main.js
+++ main.js
@@ -8,23 +8,27 @@ let title = document.getElementById("occasion")
 dateList.addEventListener("change", ()=>{
     let dayName = dateList.options[dateList.selectedIndex].textContent
     title.textContent = dayName
-    setInterval(updateTime, 200)
+    setInterval(updateTime, 40)
 })
 
 function updateTime(){
     let occasion = dateList.options[dateList.selectedIndex].value
-    let destinDate = new Date(occasion)
-    let newDate = new Date()
-    let diff = destinDate - newDate
-    let secs = Math.floor((diff / 1000) % 60)
-    let mins = Math.floor((diff / 1000 / 60) % 60)
-    let hours = Math.floor((diff / 1000 / 60 / 60) % 24)
-    let days = Math.floor(diff / 1000 / 60 / 60 / 24)
-
-    secsEl.textContent = format(secs)
-    minsEl.textContent = format(mins)
-    hoursEl.textContent = format(hours)
-    daysEl.textContent = format(days)
+    if(occasion==="none"){
+        null
+    }else{
+        let destinDate = new Date(occasion)
+        let newDate = new Date()
+        let diff = destinDate - newDate
+        let secs = Math.floor((diff / 1000) % 60)
+        let mins = Math.floor((diff / 1000 / 60) % 60)
+        let hours = Math.floor((diff / 1000 / 60 / 60) % 24)
+        let days = Math.floor(diff / 1000 / 60 / 60 / 24)
+    
+        secsEl.textContent = format(secs)
+        minsEl.textContent = format(mins)
+        hoursEl.textContent = format(hours)
+        daysEl.textContent = format(days)
+    }
 
     function format(e){
         return ("0"+e).length > 2 ? e : "0"+e
